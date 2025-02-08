2d58975eaa56877d1a563c1300161daab09e2302
Anoop Singh <anoopsing4991@gmail.com>
2025-02-09 02:23:51 +0530

    first commit

---
+++ src/main/java/com/example/kafka/KafkaExampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleApplication.class, args);
+		
+		
+		System.out.println("hello");
+	}
+
+}
